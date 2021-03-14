03aa96c1ac9a3c4e1e6dd328efa0e03d88f4a363
cristian <cristian.ortiz@uda.cl>
2021-03-14 17:02:12 -0300

    make and appóintmen form completed using hookst

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Form from "./component/Form";
 
 function App() {
+  //list of all appointemts akka allAppos
+  const [allAppos, saveAppos] = useState([]);
+
+  //function to take the actual list of appos and add a new one in the Appo State
+  const createAppo = (appo) => {
+    //the function to acces and update the AllAppos State
+
+    //copy the list of all appos , //adding the new appo in the appo State
+    saveAppos([...allAppos, appo]);
+  };
   return (
     <Fragment>
       <h1>Pets Doctor Appointment</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Form />
+            <Form createAppo={createAppo} />
           </div>
           <div className="one-half column">2</div>
         </div>
--- src/component/Form.js
+++ src/component/Form.js
@@ -1,16 +1,81 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import uuid from "uuid/dist/v4"; //for generate uniques ID
 
-const Form = () => {
+//destructuring on props object to extract createAppo
+const Form = ({ createAppo }) => {
+  //appointment aka Appo hook, State,is an object the use "({})
+  const [appo, updateAppo] = useState({
+    pet: "",
+    owner: "",
+    date: "",
+    time: "",
+    symptoms: "",
+  });
+
+  const [error, updateError] = useState(false); //is a bool this hook use only "()"
+
+  //the user enters data in a form field
+  const updateState = (e) => {
+    //the function to acces and update the appo State
+    updateAppo({
+      ...appo, //copy of appo state
+      [e.target.name]: e.target.value, //mapping form field whit user typing  input data
+    });
+  };
+
+  //user click the submit form button
+  const submitAppo = (e) => {
+    e.preventDefault();
+
+    //form validations
+    if (
+      pet.trim() === "" ||
+      owner.trim() === "" ||
+      date.trim() === "" ||
+      time.trim() === "" ||
+      symptoms.trim() === ""
+    ) {
+      updateError(true);
+      return;
+    }
+
+    //delete error msg when form valitation is correct
+    updateError(false);
+
+    //asigng and ID
+    appo.id = uuid();
+    console.log(appo);
+
+    //make the appo using the prop createAppo sent from App.js
+    createAppo(appo);
+
+    //rest the form
+    updateError({
+      pet: "",
+      owner: "",
+      date: "",
+      time: "",
+      symptoms: "",
+    });
+  };
+
+  //extracting input values from appo through destructuring
+  const { pet, owner, date, time, symptoms } = appo;
+
   return (
     <Fragment>
       <h2>Make an Appointment</h2>
-      <form>
+
+      {error ? <p className="alerta-error">All fields are mandatory</p> : null}
+      <form onSubmit={submitAppo}>
         <label>Pet's Name</label>
         <input
           type="text"
           name="pet"
           className="u-full-width" //input toma todo el espacio disponible
           placeholder="Pet's Name"
+          onChange={updateState}
+          value={pet}
         />
 
         <label>Owner's Name</label>
@@ -19,25 +84,40 @@ const Form = () => {
           name="owner"
           className="u-full-width" //input toma todo el espacio disponible
           placeholder="Owner's Name"
+          onChange={updateState}
+          value={owner}
         />
 
         <label>Date</label>
         <input
           type="date"
           name="date"
-          className="u-full-width" //input toma todo el espacio disponible
+          className="u-full-width"
+          onChange={updateState}
+          value={date}
         />
 
         <label>Time</label>
         <input
           type="time"
           name="time"
-          className="u-full-width" //input toma todo el espacio disponible
+          className="u-full-width"
+          onChange={updateState}
+          value={time}
         />
-        <label>Symptons</label>
-        <textarea className="u-full-width" name="symtomps"></textarea>
+        <label>Symptoms</label>
+        <textarea
+          className="u-full-width"
+          name="symptoms"
+          onChange={updateState}
+          value={symptoms}
+        ></textarea>
 
-        <button type="submit" className="u-full-width button-primary">
+        <button
+          type="submit"
+          className="u-full-width button-primary"
+          onChange={updateState}
+        >
           Make an Appointment
         </button>
       </form>
